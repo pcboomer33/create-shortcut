42b9612077fd8934fce43cab74f8bb2724c42394
pcboomer33 <anthony.choomnuan@gmail.com>
2024-01-27 21:39:15 +0700

    Fix last error

--- installer.py
+++ installer.py
@@ -5,14 +5,14 @@ import os
 def main():
     username = os.getlogin()
     shortcut = f"""
-    [Desktop Entry]
-    Name=Shortcut Creator
-    Exec=/usr/bin/create-shortcut
-    Icon=/home/{username}/.local/share/application/shortcut-icon.png
-    Terminal=false
-    Type=Application
-    Categories=Application
-    Hidden=false
+[Desktop Entry]
+Name=Shortcut Creator
+Exec=/usr/bin/create-shortcut
+Icon=/home/{username}/.local/share/application/shortcut-icon.png
+Terminal=false
+Type=Application
+Categories=Application
+Hidden=false
                 """
     os.system("pip3 install guizero")
     os.system("sudo cp create-shortcut.py /usr/bin/create-shortcut")
